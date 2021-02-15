a29939b32aea00aa3a56e0fc902825a62de1ae7d
David Akuma <chukay360@gmail.com>
2021-02-15 06:38:02 +0100

    netlify form added

--- src/components/Main.js
+++ src/components/Main.js
@@ -119,15 +119,20 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+          
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
+
             <div className="field half">
               <label htmlFor="email">Email</label>
               <input type="text" name="email" id="email" />
             </div>
+
             <div className="field">
               <label htmlFor="message">Message</label>
               <textarea name="message" id="message" rows="4"></textarea>
